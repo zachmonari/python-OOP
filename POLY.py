3a8c684af523cae01879e7b33937b71527a8c2c4
zach <zachmonari002@gmail.com>
2025-12-04 09:53:43 +0300

    Abstract method

--- POLY.py
+++ POLY.py
@@ -1,11 +1,25 @@
+#polymorphism
+from abc import ABC, abstractmethod
+
 class Shapes:
-    pass
+    @abstractmethod
+    def area(self)
+        pass
+
 
 class Circle(Shapes):
-    pass
+    def __init__(self, radius):
+        self.radius = radius
+
 class Triangle(Shapes):
-    pass
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
 class Rectangle(Shapes):
-    pass
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
 
 shapes=[Circle(),Triangle(),Rectangle()]
