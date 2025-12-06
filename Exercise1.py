ed98cea2eb8cb7873a1d0f81881617e63d42b3fc
zach <zachmonari002@gmail.com>
2025-12-06 21:59:07 +0300

    Ebooks

--- Exercise1.py
+++ Exercise1.py
@@ -3,11 +3,11 @@ class Book:
         return "Borrowing a general book."
 class EBook(Book):
     def borrow(self):
-        return "Downloading the eBook."
+        return "Downloading the eBook..."
 
 class AudioBook(Book):
     def borrow(self):
-        return "Streaming the audiobook."
+        return "Streaming the audiobook..."
 def process_borrow(item: Book):
     print(item.borrow())
 
