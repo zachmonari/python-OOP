6d1698189c80f4bae064154ca7aad6837e2bc37b
zach <zachmonari002@gmail.com>
2025-12-07 11:17:56 +0300

    loop

--- PaymentSystem/main.py
+++ PaymentSystem/main.py
@@ -8,3 +8,15 @@ print("Choose payment method:")
 print("1. M-Pesa")
 print("2. Card")
 print("3. PayPal")
+
+choice = input("Enter choice: ")
+amount = float(input("Enter amount: "))
+
+if choice == "1":
+    method = Mpesa()
+elif choice == "2":
+    method = Card()
+elif choice == "3":
+    method = PayPal()
+else:
+    print("Invalid option!")
