2ef7413e5b1fa96939bd7e3d800f80bf3d56fc33
zach <zachmonari002@gmail.com>
2025-12-07 11:17:14 +0300

    choices

--- PaymentSystem/main.py
+++ PaymentSystem/main.py
@@ -1,2 +1,10 @@
 from payment_methods import Mpesa, Card, PayPal
 from checkout import checkout
+
+
+print("=== MINI PAYMENT SYSTEM ===")
+
+print("Choose payment method:")
+print("1. M-Pesa")
+print("2. Card")
+print("3. PayPal")
