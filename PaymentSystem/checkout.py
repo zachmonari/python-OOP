3bf40e8f91b21e235105bd4b8f26815d4768bcf0
zach <zachmonari002@gmail.com>
2025-12-08 18:08:28 +0300

    log transactions

--- PaymentSystem/checkout.py
+++ PaymentSystem/checkout.py
@@ -1,14 +1,18 @@
 from payment_methods import Mpesa, Card, PayPal
 from receipt_generator import generate_receipt
 
-def checkout(payment_method, amount):
-    try:
-        result = payment_method.pay(amount)
-        print(result)
-        log_transaction(result)
-    except Exception as e:
-        print("Payment failed:", e)
+def checkout(payment_method, amount,currency):
+    message = payment_method.pay(amount, currency)
 
-def log_transaction(message):
+    # Log transaction
     with open("transactions.txt", "a") as f:
         f.write(message + "\n")
+
+    # PDF Receipt
+    receipt_path = generate_receipt(
+        transaction_message=message,
+        amount=amount,
+        currency=currency,
+        method_name=payment_method.__class__.__name__
+    )
+    return message, receipt_path
