ba1b77e863a0ee7249be39e0a02745a789b392b4
zach <zachmonari002@gmail.com>
2025-12-10 20:04:26 +0300

    Main code for database

--- PaymentSystem/database.py
+++ PaymentSystem/database.py
@@ -1,1 +1,9 @@
-import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///payments.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
