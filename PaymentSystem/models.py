eae99097bdf2189ecabc125bcb7edb0f38d6f8f3
zach <zachmonari002@gmail.com>
2025-12-10 20:06:34 +0300

    Transactions model

---
+++ PaymentSystem/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, Float, String, DateTime
+from database import Base
+import datetime
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    method = Column(String, nullable=False)
+    amount = Column(Float, nullable=False)
+    currency = Column(String, nullable=False)
+    receipt_path = Column(String)
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+    status = Column(String, default="SUCCESS")  # future: FAILED, PENDING
